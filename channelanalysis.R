e74291720ee01caa93898bd902920eaf0cac12e1
Learning2Code-15 <starkalaivani@gmail.com>
2020-04-24 02:12:55 +0530

    Top 20 videos from 3 channels - code added

--- channelanalysis.R
+++ channelanalysis.R
@@ -16,8 +16,8 @@ library(writexl)
 
 
 
-yt_oauth(app_id = "863216899807-866730r6aeuuf7p68vk2ck5n78dss6pk.apps.googleusercontent.com",
-         app_secret = "HM8HMczhZi3DuTg92ch4CvQv",token = "")
+yt_oauth(app_id = "672457131068-fhqsi5fcdn1un01hbuj835dqetaneliv.apps.googleusercontent.com",
+         app_secret = "TOyAaxGjxF5COZuS_49dgH19",token = "")
 #Prepare a list of channelId's
 channel_id<- c("UCwAdQUuPT6laN-AQR17fe1g","UCe3qdG0A_gr-sEdat5y2twQ","UCbOkZlwRtOP3EGGAYd-Zi1Q")
 channel_analysis=data.frame(channel_id,stringsAsFactors = TRUE)
@@ -25,4 +25,36 @@ channel_analysis=lapply(as.character(channel_analysis$channel_id), function(x){
   get_channel_stats(channel_id =x)
 })
 channel_analysis <- ldply(channel_analysis,data.frame)
-channel_analysis_res$id=as.character(channel_analysis_res$id) 
+channel_analysis$id=as.character(channel_analysis$id) 
+
+#excelsheet=write_xlsx(channel_analysis,"channelanalysis.xlsx")
+#To view channel analysis in a neat format
+channel_analysis_tab=data.frame("Title"=channel_analysis$snippet.title,
+                                "View Count"=channel_analysis$statistics.viewCount,
+                                "Subscriber Count"=channel_analysis$statistics.subscriberCount,
+                                "Video Count"=channel_analysis$statistics.videoCount)
+kable_styling(kable(channel_analysis_tab))
+
+#Top 20 videos in all channel and top 3 in each channel colr scheme channel
+videostats_All=data.frame(matrix(ncol = 0,nrow = 0))
+for(i in 1:3) {
+  videostats=get_all_channel_video_stats(channel_analysis$id[i])
+  videostats$chid[1:nrow(videostats)]=channel_analysis$id[i]
+  videostats$chtitle[1:nrow(videostats)]=channel_analysis$id[i]
+  videostats_All=rbind(videostats_All,videostats)
+  } 
+videostats_All=videostats_All[!duplicated(videostats_All[,c('id')]),]
+excelsheet=write_xlsx(list(videostats_sheet=videostats_All,channelstats_sheet=channel_analysis),excelsheet)
+videostats_All <- merge(videostats_All,channel_analysis[,c("id","snippet.title")], by.x = "channelid",by.y = "id",sort = F, all.x = T)
+
+videostats_All$likeCount=as.numeric(videostats_All$likeCount)
+videostats_All$viewCount=as.numeric(videostats_All$viewCount)
+
+Top_Liked_all=tail(videostats_All[order(videostats_All$likeCount),],20)
+Top_Viewed_all=tail(videostats_All[order(videostats_All$viewCount),],20)
+Top_Viewed_all$channelid=as.factor(Top_Viewed_all$channelid)
+Viewedplot=ggplot(Top_Viewed_all, aes(x=viewCount, y=title, fill=snippet.title)) +
+  geom_bar(stat="identity", width=0.75,position = position_dodge(width=0.05))+theme_minimal()
+Viewedplot=Viewedplot + theme(axis.text = element_text(size=7.5) ,axis.text.x=element_blank(),axis.ticks.x=element_blank(),axis.title.y=element_blank())+ labs(x ="Number of Views",fill="CHANNEL TITLE")+scale_fill_brewer(palette = "Set2")
+Viewedplot
+#Word cloud for channel
